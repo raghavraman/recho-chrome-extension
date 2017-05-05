013dc0c868f4a79b6c5b0c0b8847c38fed3771c3
Raghav-zarget <Raghav@Raghav.local>
2017-05-06 00:03:07 +0530

    GOT THE COOKIE

--- popup.js
+++ popup.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+  chrome.cookies.get({ url: 'http://recho.com/', name: '_ot' },
+  function (cookie) {
+    if (cookie) {
+      console.log(cookie.value);
+    }
+    else {
+      console.log('Can\'t get cookie! Check the name!');
+    }
+  });
+
+
   var checkPageButton = document.getElementById('saveToAPI');
   checkPageButton.addEventListener('click', function() {
     chrome.tabs.getSelected(null,function(tab) {
