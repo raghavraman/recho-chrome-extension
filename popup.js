649005663f022d757a66bca10c8dff7779e712bb
Raghav-zarget <Raghav@Raghav.local>
2017-05-06 00:32:25 +0530

    GOT THE COOKIE version 2

--- popup.js
+++ popup.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  chrome.cookies.get({ url: 'http://recho.com/', name: '_ot' },
-  function (cookie) {
-    if (cookie) {
-      console.log(cookie.value);
-    }
-    else {
-      console.log('Can\'t get cookie! Check the name!');
-    }
-  });
+    chrome.cookies.get({ url: 'http://recho.com/', name: '_ot' },
+        function(cookie) {
+            if (cookie) {
+                console.log(cookie.value);
+            } else {
+                chrome.tabs.getSelected(null, function(tab) {
+                    chrome.tabs.update(tab.id, { url: "http://recho.com" });
+                });
+
+            }
+        });
 
 
-  var checkPageButton = document.getElementById('saveToAPI');
-  checkPageButton.addEventListener('click', function() {
-    chrome.tabs.getSelected(null,function(tab) {
-    var tablink = tab.url;
-    console.log(tablink)
-    });
-  }, false);
+    var checkPageButton = document.getElementById('saveToAPI');
+    checkPageButton.addEventListener('click', function() {
+        chrome.tabs.getSelected(null, function(tab) {
+            var tablink = tab.url;
+            console.log(tablink)
+        });
+    }, false);
 }, false);
