fed1121686b7746881e1a8012202d1e72239ad05
Raghav-zarget <Raghav@Raghav.local>
2017-05-06 17:37:43 +0530

    chrome plugin version 1

--- popup.js
+++ popup.js
@@ -1,23 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    chrome.cookies.get({ url: 'http://recho.com/', name: '_ot' },
+  var cookval;
+    chrome.cookies.get({ url: 'https://www.rekho.com/', name: '_ot' },
         function(cookie) {
             if (cookie) {
                 console.log(cookie.value);
+                cookval=cookie.value;
             } else {
                 chrome.tabs.getSelected(null, function(tab) {
-                    chrome.tabs.update(tab.id, { url: "http://recho.com" });
+                    chrome.tabs.update(tab.id, { url: "http://rekho.com/test.html" });
                 });
-
             }
         });
 
 
     var checkPageButton = document.getElementById('saveToAPI');
+    checkPageButton.style.display = 'block';
+    var responseText=document.getElementById('successSave');
     checkPageButton.addEventListener('click', function() {
         chrome.tabs.getSelected(null, function(tab) {
             var tablink = tab.url;
-            console.log(tablink)
+                //send request tot api
+            $.ajax({
+                url: "https://api.rekho.com/v1/articles/",
+                type: "POST",
+                dataType: "json",
+                headers: {
+                    "Authorization": "token " + cookval
+                },
+                data: {
+                    "url": tablink
+                },
+                success: function(data) {
+                    responseText.innerHTML="Added to your listening list";
+                    checkPageButton.style.display = 'none';
+                },
+                error: function(e) {
+                  responseText.innerHTML="SORRY    " +e.responseJSON.url.join(", ");
+                    console.log(e);
+                    checkPageButton.style.display = 'none';
+                }
+            });
         });
     }, false);
 }, false);
+
